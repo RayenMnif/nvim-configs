6d68a723c49c4cf0689957851e463471fc6c396a
RayenMnif <mnif.rayen07@gmail.com>
2024-05-01 16:42:02 +0100

    simple changes

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -3,29 +3,19 @@ local dashboard = require("alpha.themes.dashboard")
 
 -- Set header
 dashboard.section.header.val = {
+    "","",
+    "","",
+    "","",
+    "","",
+    "","",
     "▄▄███▄▄·██████╗  ██████╗ ███╗   ██╗██╗   ██╗",
     "██╔════╝██╔══██╗██╔═══██╗████╗  ██║╚██╗ ██╔╝",
     "███████╗██████╔╝██║   ██║██╔██╗ ██║ ╚████╔╝ ",
     "╚════██║██╔══██╗██║   ██║██║╚██╗██║  ╚██╔╝  ",
-    "███████║██║  ██║╚██████╔╝██║ ╚████║   ██║   ",  
+    "███████║██║  ██║╚██████╔╝██║ ╚████║   ██║   ",
     "╚═▀▀▀══╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ",
-    "                                            ",
+    "","",
 }
-
---   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
---   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
---   ```init.lua
---   return require('packer').startup(function()
---       use 'wbthomason/packer.nvim'
---       use {
---           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
---           requires = {'BlakeJC94/alpha-nvim-fortune'},
---           config = function() require("config.alpha") end
---       }
---   end)
---   ```
--- local fortune = require("alpha.fortune") 
--- dashboard.section.footer.val = fortune()
 
 -- Send config to alpha
 alpha.setup(dashboard.opts)
