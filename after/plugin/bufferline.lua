5dd042412e77d7af141f35a94a476afb1cda9968
RayenMnif <mnif.rayen07@gmail.com>
2024-05-07 19:57:16 +0100

    improving the bufferLine

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,33 +1,27 @@
 require("bufferline").setup{
     options = {
         always_show_bufferline = false,
-        show_buffer_close_icons = false,
+        show_buffer_close_icons = true,
         show_close_icon = false,
-        color_icons = true
-    },
-    highlights = {
-        separator = {
-            guifg = '#073642',
-            guibg = '#002b36'
-        },
-        separator_selected = {
-            guifg = '#073642'
-        },
-        background = {
-            guifg = '#657b83',
-            guibg = '#002b36'
-        },
-        buffer_selected = {
-            guifg = '#fdf6e3',
-            gui = 'bold'
-        },
-        fill = {
-            guibg = '#073642'
+        color_icons = true,
+        diagnostics = "nvim_lsp" ,
+        tab_size = 26,
+        close_command = "bdelete!", 
+        show_tab_indicators = true ,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = function()
+                    return vim.fn.getcwd()
+                end,
+                highlight = "Directory",
+                text_align = "left"
+
+            }
         }
-    }
+    },
 }
 
 vim.api.nvim_set_keymap("n", '<Tab>', '<cmd>BufferLineCycleNext<CR>', {} )
 vim.api.nvim_set_keymap("n", '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', {} )
-
-
+vim.api.nvim_set_keymap("n", '<S-w>', '<cmd>bdelete<CR>', {} )
